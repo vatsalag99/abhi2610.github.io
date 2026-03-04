14767194d6df2b3fc527769f81b7ddb52fdf5c57
Abhinav Shrivastava <880094+abhi2610@users.noreply.github.com>
2026-03-03 21:38:32 -0500

    Update site URL to abhinavsh.info, add MAPS PDF link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://abhi2610.github.io',
+  site: 'https://abhinavsh.info',
   output: 'static',
   integrations: [sitemap()],
 });
